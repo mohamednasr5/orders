59f55083e17bc8adcc0acef08166d698f78d74cb
mohamednasr5 <elfannanm@gmail.com>
2026-08-05 16:36:59 +0300

    1
    1

--- js - Copy/auth.js
+++
@@ -1,9 +0,0 @@
-export function initAuth() {
-    const loginBtn = document.getElementById('btn-login');
-    if (loginBtn) {
-        loginBtn.addEventListener('click', () => {
-            console.log('Initiating Google Sign-in via Firebase...');
-            alert("سيتم تنفيذ تسجيل الدخول بجوجل هنا (Firebase Config)");
-        });
-    }
-}
